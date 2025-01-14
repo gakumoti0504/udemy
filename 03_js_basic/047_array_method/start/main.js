7cdcac9ad271eb8b7e3b03cc7620a986eee2ec2a
gakumoti0504 <af20029@shibaura-it.ac.jp>
2025-01-14 11:31:00 +0900

    🚧 mapとfilter

--- 03_js_basic/047_array_method/start/main.js
+++ 03_js_basic/047_array_method/start/main.js
@@ -1,2 +1,13 @@
 const arry = [10, 20, 30, 40];
-const newArry = [];
+const newarry = [];
+
+for (let i=0;i<arry.length;i++){
+    const val = arry[i]*2;
+    if(val>50){
+        newarry.push(val);
+    }
+    console.log(newarry);
+}
+
+const newarry2 = arry.map((val)=>(val*2)).filter((val)=>(val>50));
+console.log(newarry2);
